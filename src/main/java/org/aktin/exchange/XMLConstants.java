d9546276491054868c63a5f7b70370a9f16e4bc7
R.W.Majeed <raphael@majeed.de>
2016-09-26 21:31:12 +0200

    JAXB classes for query exchange moved to project broker-api

--- src/main/java/org/aktin/exchange/XMLConstants.java
+++
@@ -1,9 +0,0 @@
-package org.aktin.exchange;
-
-import javax.xml.bind.annotation.XmlTransient;
-
-@XmlTransient
-public class XMLConstants {
-
-	public static final String XML_NAMESPACE="http://aktin.org/ns/exchange";
-}
