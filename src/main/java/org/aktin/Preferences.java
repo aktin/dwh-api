a483e82ff00c5d5dcdf47680ed553f924ae913a0
R.W.Majeed <raphael@majeed.de>
2016-11-01 12:19:35 +0100

    allow get via enum

--- src/main/java/org/aktin/Preferences.java
+++ src/main/java/org/aktin/Preferences.java
@@ -2,9 +2,14 @@ package org.aktin;
 
 import java.util.Set;
 
+import org.aktin.dwh.PreferenceKey;
+
 public interface Preferences {
 
 	String get(String key);
 
+	default String get(PreferenceKey key){
+		return get(key.key());
+	}
 	Set<String> keySet();
 }
