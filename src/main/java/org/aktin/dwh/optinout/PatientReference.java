f4d5fef8206e43055780fd81cbd7b5af2d05b1df
Corinna Feeken <corinna.feeken1@uni-oldenburg.de>
2018-11-29 18:01:17 +0100

    information about location of root and extension in CDA document.

--- src/main/java/org/aktin/dwh/optinout/PatientReference.java
+++ src/main/java/org/aktin/dwh/optinout/PatientReference.java
@@ -1,12 +1,20 @@
 package org.aktin.dwh.optinout;
 
 public enum PatientReference {
-	/** patient specific id */
+	/** patient specific id 
+	 * CDA element: /ClinicalDocument/recordTarget/patientRole/id
+	 */
 	Patient,
-	/** visit id, a visit can contain multiple encounters */
+	/** visit id, a visit can contain multiple encounters 
+	 * Not used in this project.
+	 */
 	Visit,
-	/** encounter (with a practitioner) */
+	/** encounter (with a practitioner) 
+	 * CDA element: /ClinicalDocument/componentOf/encompassingEncounter/id[1]
+	 */
 	Encounter,
-	/** billing / accounting number of the hospital, used for financial transactions */
+	/** billing / accounting number of the hospital, used for financial transactions 
+	 * CDA element: /ClinicalDocument/componentOf/encompassingEncounter/id[2]
+	 */
 	Billing
 }
